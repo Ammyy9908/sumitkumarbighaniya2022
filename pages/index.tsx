08c9d4ff5176cd7408f58e9eaee5ac44d49e296a
ammyy9908 <sb78639@gmail.com>
2022-06-24 15:26:18 +0530

    coffee button added

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,8 @@ function Home({dark}) {
         <meta name="description" content="Frontend Web Develoepr based in Bangalore,India" />
         <meta name="keywords" content="HTML, CSS, JavaScript,Sumit Website,Netflix Web Clones,Frontend Web developer Bangalore,Web Clones ReactJS Developer Bangalore,India,Instagram Clone ReactJS,NextJs"/>
   <meta name="author" content="Sumit Kumar"/>
-
+  
+  <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="sb78639V" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting! now you can buy me a coffee!" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         <link rel="icon" href="/favicon.ico" />
         
       </Head>
@@ -224,6 +225,9 @@ style={{
 >
   <MdMailOutline/> sb78639@gmail.com
 </a>
+
+
+
       </div>
 
       <Footer dark={dark}/>
